4d00d990813d267ad03b98daed13ca6abe285afb
Sinisa Habek <sinisa@habek.net>
2023-04-26 12:57:33 +0200

    Refactor in preparation for moving input into hook
    Move Compute from calculator into utils.
    Add Calculator/Entry tests to useCalculator hook.

--- src/calculator/Calculator.ts
+++ src/calculator/Calculator.ts
@@ -21,7 +21,7 @@ import {
 } from "../constants";
 
 import Entry from "./entry/Entry";
-import Compute from "./compute/Compute";
+import Compute from "../utils/Compute";
 
 /**
  * Implement calculator by displaying entered or computed value as result of
--- src/hooks/useCalculator.test.tsx
+++ src/hooks/useCalculator.test.tsx
@@ -1,22 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { KEY_3, KEY_5, KEY_OP_ADD, KEY_OP_EQUALS } from "../constants";
+import Display from "../components/display/Display";
+import Keypad from "../components/keypad/Keypad";
+
 import Calculator from "../calculator/Calculator";
 import useCalculator from "./useCalculator";
 
 function TestComponent({ calculator }: { calculator: Calculator }) {
-  const [displayed, punch] = useCalculator(calculator);
+  const [displayed, punchKey] = useCalculator(calculator);
 
   return (
     <>
-      <div title="display">{displayed}</div>
-      <div>
-        <button onClick={() => punch(KEY_3)}>3</button>
-        <button onClick={() => punch(KEY_5)}>5</button>
-        <button onClick={() => punch(KEY_OP_ADD)}>+</button>
-        <button onClick={() => punch(KEY_OP_EQUALS)}>=</button>
-      </div>
+      <Display displayed={displayed} />
+      <Keypad onKeyPunch={(key) => punchKey(key)} />
     </>
   );
 }
@@ -30,23 +27,207 @@ describe("useCalculator", () => {
 
   test("display initial value", () => {
     render(<TestComponent calculator={calculator} />);
-    expect(screen.getByTitle("display")).toHaveTextContent("0");
+    expect(screen.getByTitle("Display")).toHaveTextContent("0");
   });
 
-  test("update display when key is punched", async () => {
+  test("capture input", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "1" }));
+    expect(screen.getByTitle("Display")).toHaveTextContent("1");
+
+    await user.click(screen.getByRole("button", { name: "2" }));
+    expect(screen.getByTitle("Display")).toHaveTextContent("12");
+
+    await user.click(screen.getByRole("button", { name: "." }));
+    expect(screen.getByTitle("Display")).toHaveTextContent("12.");
+
+    await user.click(screen.getByRole("button", { name: "3" }));
+    expect(screen.getByTitle("Display")).toHaveTextContent("12.3");
+  });
+
+  test("allow max 15 characters in input", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "1" }));
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+    await user.click(screen.getByRole("button", { name: "4" }));
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "6" }));
+    await user.click(screen.getByRole("button", { name: "7" }));
+    await user.click(screen.getByRole("button", { name: "8" }));
+    await user.click(screen.getByRole("button", { name: "9" }));
+    await user.click(screen.getByRole("button", { name: "0" }));
+    await user.click(screen.getByRole("button", { name: "." }));
+    await user.click(screen.getByRole("button", { name: "1" }));
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+    await user.click(screen.getByRole("button", { name: "4" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("1234567890.1234");
+
+    await user.click(screen.getByRole("button", { name: "5" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("1234567890.1234");
+  });
+
+  test("allow only one decimal separator in input", async () => {
     const user = userEvent.setup();
     render(<TestComponent calculator={calculator} />);
 
     await user.click(screen.getByRole("button", { name: "5" }));
-    expect(screen.getByTitle("display")).toHaveTextContent("5");
+    await user.click(screen.getByRole("button", { name: "." }));
+    await user.click(screen.getByRole("button", { name: "2" }));
 
-    await user.click(screen.getByRole("button", { name: "+" }));
-    expect(screen.getByTitle("display")).toHaveTextContent("5");
+    expect(screen.getByTitle("Display")).toHaveTextContent("5.2");
+
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "." }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("5.25");
+  });
+
+  test("add numbers", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
 
     await user.click(screen.getByRole("button", { name: "3" }));
-    expect(screen.getByTitle("display")).toHaveTextContent("3");
+    await user.click(screen.getByRole("button", { name: "7" }));
+    await user.click(screen.getByRole("button", { name: "+" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("37");
+
+    await user.click(screen.getByRole("button", { name: "1" }));
+    await user.click(screen.getByRole("button", { name: "5" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("15");
 
     await user.click(screen.getByRole("button", { name: "=" }));
-    expect(screen.getByTitle("display")).toHaveTextContent("8");
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("52");
+  });
+
+  test("subtract numbers", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "1" }));
+    await user.click(screen.getByRole("button", { name: "6" }));
+    await user.click(screen.getByRole("button", { name: "-" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("16");
+
+    await user.click(screen.getByRole("button", { name: "9" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("9");
+
+    await user.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("7");
+  });
+
+  test("multiply numbers", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "." }));
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "×" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("2.5");
+
+    await user.click(screen.getByRole("button", { name: "1" }));
+    await user.click(screen.getByRole("button", { name: "." }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("1.3");
+
+    await user.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("3.25");
+  });
+
+  test("divide numbers", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "÷" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("5");
+
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("2.5");
+  });
+
+  test("calculate percent", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "4" }));
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "%" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("0.45");
+  });
+
+  test("clear all", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "+" }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+    await user.click(screen.getByRole("button", { name: "AC" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("0");
+
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("5"); // ignore previous input
+  });
+
+  test("clear input", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "+" }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+    await user.click(screen.getByRole("button", { name: "C" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("0");
+
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("7"); // clear only last input (3)
+  });
+
+  test("chain multiple operations", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent calculator={calculator} />);
+
+    await user.click(screen.getByRole("button", { name: "6" }));
+    await user.click(screen.getByRole("button", { name: "+" }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+    await user.click(screen.getByRole("button", { name: "÷" }));
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "×" }));
+    await user.click(screen.getByRole("button", { name: "2" }));
+    await user.click(screen.getByRole("button", { name: "." }));
+    await user.click(screen.getByRole("button", { name: "5" }));
+    await user.click(screen.getByRole("button", { name: "C" }));
+    await user.click(screen.getByRole("button", { name: "3" }));
+    await user.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTitle("Display")).toHaveTextContent("13.5");
   });
 });
